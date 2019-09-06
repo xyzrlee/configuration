86effa0134d30d5eadd4ef0b6001836e223ce782
Ricky Li <cnrickylee@gmail.com>
2019-09-06 13:36:04 +0800

    test

--- script/surge-ad.sh
+++ script/surge-ad.sh
@@ -7,12 +7,14 @@ script_path=$(cd `dirname $0`; pwd)
 pushd ${script_path}
 rm -rf ../venv
 python3 -m venv ../venv
-../venv/bin/pip3 install wheel
-../venv/bin/pip3 install -r python/adblockplus2surge/requirements.txt
+source ../venv/bin/activate
+pip3 install wheel
+pip3 install -r python/adblockplus2surge/requirements.txt
 mkdir -p ../auto/surge
-../venv/bin/python3 python/run_adblockplus2surge.py \
+python3 python/run_adblockplus2surge.py \
   --output-file=../auto/surge/ad.list \
   --abp-rule=https://easylist-downloads.adblockplus.org/easylistchina.txt \
   --abp-rule=https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt \
   --abp-rule=https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ABP-FX.txt
+deactivate
 popd
