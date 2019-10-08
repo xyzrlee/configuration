9c83f5da3d8319e9b2f79152b90b40cae8951667
Ricky Li <cnrickylee@gmail.com>
2019-10-08 10:55:40 +0800

    add dnsmasq conf (#9)

---
+++ script/dnsmasq-china-domains.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### use script from https://github.com/felixonmars/dnsmasq-china-list
+
+set -e
+
+script_path=$(cd `dirname $0`; pwd)
+pushd ${script_path}/..
+
+DIR="$(mktemp -d)"
+git clone --depth=1 https://github.com/felixonmars/dnsmasq-china-list.git ${DIR}
+pushd ${DIR}
+make SERVER=223.5.5.5,223.6.6.6,119.29.29.29 dnsmasq
+popd
+mkdir -p auto/dnsmasq
+cp ${DIR}/accelerated-domains.china.dnsmasq.conf auto/dnsmasq/china-domains.conf
+
+popd
