611989e97c5ee08262e9811f4567ebd830bfc82a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-05 13:01:41 +0000

    Deploy xyzrlee/configuration to github.com/xyzrlee/configuration.git:gh-pages

--- script/python/surge_ad.py
+++ script/python/surge_ad.py
@@ -1,4 +1,5 @@
 import getopt
+import os
 import sys
 
 from adblockplus2lists import AdblockPlus2Lists, Result, Rules
@@ -6,20 +7,24 @@ from adblockplus2lists import AdblockPlus2Lists, Result, Rules
 
 def main(argv) -> None:
     output_file = None
+    type = "list"
     rules = Rules()
-    opts, args = getopt.getopt(argv, "", ["output-file=", "abp-rule="])
+    opts, args = getopt.getopt(argv, "", ["output-file=", "abp-rule=", "type="])
     for opt, arg in opts:
         if opt == "--output-file":
             output_file = arg
         elif opt == "--abp-rule":
             rules.add(arg)
+        elif opt == "--type":
+            type = arg
     obj = AdblockPlus2Lists(rules)
     result: Result = obj.convert()
-    if output_file:
-        with open(output_file, "w") as ouf:
+    print("%s" % os.path.abspath(output_file))
+    with open(output_file, "w") as ouf:
+        if type == "list":
             result.write_surge(file=ouf)
-    else:
-        result.write_surge()
+        elif type == "set":
+            result.write_surge_domain_set(file=ouf)
     result.print_statistics()
 
 
