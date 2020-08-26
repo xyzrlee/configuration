5da01188582ae5bdaf1374a4071f29dba1625d89
Ricky Li <cnrickylee@gmail.com>
2020-08-26 15:19:26 +0800

    更新dns服务IP替换方式 (#13)
    * 更新dns服务IP替换方式
    
    * fix travis warnings

--- script/dnscrypt-proxy-forwarding.sh
+++ script/dnscrypt-proxy-forwarding.sh
@@ -2,17 +2,21 @@
 
 ### use script from https://github.com/felixonmars/dnsmasq-china-list
 
+DNS_PLACEHOLDER="===DNS==="
+DNS_SERVERS="223.5.5.5 223.6.6.6 119.29.29.29"
+
 set -e
 
-script_path=$(cd `dirname $0`; pwd)
+script_path=$(cd "$(dirname "$0")"; pwd)
 pushd ${script_path}/..
 
 DIR="$(mktemp -d)"
 git clone --depth=1 https://github.com/felixonmars/dnsmasq-china-list.git ${DIR}
 pushd ${DIR}
-make SERVER=223.5.5.5,223.6.6.6,119.29.29.29 dnscrypt-proxy
+make SERVER="${DNS_PLACEHOLDER}" dnscrypt-proxy
 popd
 mkdir -p auto/dnscrypt-proxy
 cp ${DIR}/dnscrypt-proxy-forwarding-rules.txt auto/dnscrypt-proxy/forwarding-rules.txt
+sed -i -e "s/${DNS_PLACEHOLDER}/ ${DNS_SERVERS}/g" auto/dnscrypt-proxy/forwarding-rules.txt
 
 popd
