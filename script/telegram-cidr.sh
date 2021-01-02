0e9a7a2a718f9e95b70393dc7879b6c317d8bcde
xyzrlee <cnrickylee@gmail.com>
2021-01-02 18:31:49 +0800

    add telegram-cidr

---
+++ script/telegram-cidr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+script_path=$(cd "$(dirname "$0")"; pwd)
+pushd ${script_path}/..
+
+mkdir -p auto/telegram
+curl 'https://core.telegram.org/resources/cidr.txt' | awk '{ gsub(/[ \t]+$/, "", $0); p = "IP-CIDR"; if ($0 ~ ":") p = "IP-CIDR6"; printf("%s,%s,no-resolve\n", p, $0) }' >auto/telegram/telegram-cidr.list
+
+popd
