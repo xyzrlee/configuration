ec5108e82353fb9111088d572a84beb7a5a68801
Ricky Li (via Travis CI) <cnrickylee@gmail.com>
2021-01-02 10:50:08 +0000

    Deploy xyzrlee/configuration to github.com/xyzrlee/configuration.git:gh-pages

--- script/telegram-cidr.sh
+++ script/telegram-cidr.sh
@@ -5,7 +5,7 @@ set -e
 script_path=$(cd "$(dirname "$0")"; pwd)
 pushd ${script_path}/..
 
-mkdir -p auto/telegram
-curl 'https://core.telegram.org/resources/cidr.txt' | awk '{ gsub(/[ \t]+$/, "", $0); p = "IP-CIDR"; if ($0 ~ ":") p = "IP-CIDR6"; printf("%s,%s,no-resolve\n", p, $0) }' >auto/telegram/telegram-cidr.list
+mkdir -p auto/surge
+curl 'https://core.telegram.org/resources/cidr.txt' | awk '{ gsub(/[ \t]+$/, "", $0); p = "IP-CIDR"; if ($0 ~ ":") p = "IP-CIDR6"; printf("%s,%s,no-resolve\n", p, $0) }' >auto/surge/telegram-cidr.list
 
 popd
