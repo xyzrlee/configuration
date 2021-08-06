e64448d25d0f5e9841d5073847c6e736fa764983
Ricky Li <cnrickylee@gmail.com>
2021-08-06 21:35:07 +0800

    workflow repository dispatch (#21)

---
+++ script/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+script_path=$(cd `dirname $0`; pwd)
+pushd ${script_path}
+
+chmod +x *.sh
+
+./venv.sh
+./gitignore.sh
+./surge-ad.sh
+./surge-gfwlist.sh
+./dnscrypt-proxy-forwarding.sh
+./privoxy-gfwlist.sh
+./chinadns-chnroute.sh
+./dnsmasq-china-domains.sh
+./telegram-cidr.sh
+
+popd
