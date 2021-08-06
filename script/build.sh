17506ff11f6468c9eb6dc519b04e0eb9e03c35d6
xyzrlee <cnrickylee@gmail.com>
2021-08-06 13:40:44 +0000

    Deploying to gh-pages from @ xyzrlee/configuration@15238824d5c4d9873df8431dadfdc66ccc81b2ac 🚀

---
+++ script/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+script_path=$(cd `dirname $0`; pwd)
+pushd ${script_path}
+
+chmod +x *.sh
+
+./venv.sh
+./surge-ad.sh
+./surge-gfwlist.sh
+./dnscrypt-proxy-forwarding.sh
+./privoxy-gfwlist.sh
+./chinadns-chnroute.sh
+./dnsmasq-china-domains.sh
+./telegram-cidr.sh
+
+popd
