021d6a17d15cb56f4d51c377a2c20252c990d447
xyzrlee <cnrickylee@gmail.com>
2021-08-06 11:07:25 +0800

    fix

--- script/build.sh
+++ script/build.sh
@@ -3,18 +3,18 @@
 set -e
 
 script_path=$(cd `dirname $0`; pwd)
-pushd ${script_path}/..
+pushd ${script_path}
 
 chmod +x *.sh
 
-venv.sh
-gitignore.sh
-surge-ad.sh
-surge-gfwlist.sh
-dnscrypt-proxy-forwarding.sh
-privoxy-gfwlist.sh
-chinadns-chnroute.sh
-dnsmasq-china-domains.sh
-telegram-cidr.sh
+./venv.sh
+./gitignore.sh
+./surge-ad.sh
+./surge-gfwlist.sh
+./dnscrypt-proxy-forwarding.sh
+./privoxy-gfwlist.sh
+./chinadns-chnroute.sh
+./dnsmasq-china-domains.sh
+./telegram-cidr.sh
 
 popd
